ebebcf69deb49891217bf0be7a9274366075b7b1
Bo Opfer <boopfer@gmail.com>
2020-11-15 17:46:04 +0700

    git push

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,9 @@
+// module.exports = {
+//     mongoURI:'mongodb://boopfer:Snooker1@ds215709.mlab.com:15709/botestdb',
+//     secretOrKey: 'secret'
+// };
+
 module.exports = {
-    mongoURI:'mongodb://boopfer:Snooker1@ds215709.mlab.com:15709/botestdb'
+  mongoURI: "mongodb://localhost:27017/bo_database",
+  secretOrKey: "secret"
 };
